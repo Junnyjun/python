8c01b2e5fd5f30c3bcad514a9fb31d7adc007428
pyungpyang <thlee1229@gmail.com>
2022-04-28 23:16:13 +0900

    feat(계산기) : 계산기 생성 기초

---
+++ src/main/Calculator.py
@@ -0,0 +1,15 @@
+class  calculator:
+    def __init__(self):
+        self.data = 0
+
+    def add(self, add_number):
+        self.data += add_number
+
+    def minus(self, add_number):
+        self.data -= add_number
+
+    def multi(self, add_number):
+        self.data *= add_number
+
+    def div(self, add_number):
+        self.data /= add_number
