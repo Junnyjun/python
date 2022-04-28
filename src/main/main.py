da64f9a3cb3f0292c69d4c9941e05434a0924802
pyungpyang <thlee1229@gmail.com>
2022-04-28 23:15:41 +0900

    feat(메인클래스 생성) : 계산기 만들기 기초

---
+++ src/main/main.py
@@ -0,0 +1,15 @@
+import Calculator, entry
+
+if __name__ == '__main__':
+    print("HELLO WORLD")
+    Calculator.calculator
+    entry.inputdatas
+    a = Calculator.calculator()
+    b = entry.inputdata()
+
+    a.add()
+    b.inputvalue()
+
+    a.data
+    b.expression
+
